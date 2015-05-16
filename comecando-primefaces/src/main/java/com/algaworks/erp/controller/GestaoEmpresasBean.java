f9d2ef623c5deed2ca721851e560f692e5f3e0bf
LUCAS MOREIRA <eskerlucas1@gmail.com>
2015-05-16 13:30:26 -0300

    incluindo um formulario na modal de empresas

--- comecando-primefaces/src/main/java/com/algaworks/erp/controller/GestaoEmpresasBean.java
+++ comecando-primefaces/src/main/java/com/algaworks/erp/controller/GestaoEmpresasBean.java
@@ -8,7 +8,9 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import com.algaworks.erp.model.Empresa;
+import com.algaworks.erp.model.TipoEmpresa;
 import com.algaworks.erp.repository.Empresas;
+import com.algaworks.erp.service.CadastroEmpresaService;
 
 @Named
 @ViewScoped
@@ -19,14 +21,39 @@ public class GestaoEmpresasBean implements Serializable {
 	@Inject
 	private Empresas empresas;
 	
+	@Inject
+	private CadastroEmpresaService cadastroEmpresaService;
+	
 	private List<Empresa> todasEmpresas;
+	private Empresa empresa = new Empresa();
+	
+	public void prepararNovoCadastro() {
+		empresa = new Empresa();
+	}
+	
+	public void salvar() {
+		cadastroEmpresaService.salvar(empresa);
+		consultar();
+	}
 	
 	public void consultar() {
 		todasEmpresas = empresas.todas();
 	}
 	
+	public TipoEmpresa[] getTiposEmpresas() {
+		return TipoEmpresa.values();
+	}
+	
 	public List<Empresa> getTodasEmpresas() {
 		return todasEmpresas;
+	}
+
+	public Empresa getEmpresa() {
+		return empresa;
+	}
+
+	public void setEmpresa(Empresa empresa) {
+		this.empresa = empresa;
 	}
 	
 	
